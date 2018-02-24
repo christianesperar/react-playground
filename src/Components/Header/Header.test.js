7381c3208971f6c147482015375527db858d7db7
christianesperar <christianesperar@gmail.com>
2018-02-25 03:55:16 +0800

    Add chai-enzyme

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { expect } from './../../utils/chai';
 import Header from './Header';
 
 it('should show the title', () => {
   const wrapper = mount(<Header title='Welcome to React Playground' />);
   const title = wrapper.find('h1').text();
 
-  expect(title).toBe('Welcome to React Playground');
+  expect(title).to.equal('Welcome to React Playground');
 });
---
+++ src/utils/chai.js
@@ -0,0 +1,6 @@
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+
+chai.use(chaiEnzyme());
+
+export const expect = chai.expect;
