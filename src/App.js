c3a9ea57b1758e408a6874aad1f3aaf4f7a57769
christianesperar <christianesperar@gmail.com>
2018-02-25 00:44:51 +0800

    Move Header to Components directory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Header from './Header/Header';
+import Header from './Components/Header/Header';
 import './App.css';
 
 class App extends Component {
