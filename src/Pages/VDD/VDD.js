bea0521eb333f9ea455be48a866bc136f59371bd
christianesperar <christianesperar@gmail.com>
2018-03-20 17:48:51 +0800

    Fix Loading component not unmounting

--- src/Pages/VDD/VDD.js
+++ src/Pages/VDD/VDD.js
@@ -4,36 +4,42 @@ import Description from './../../Components/Description/Description';
 import Loading from './../../Components/Loading/Loading';
 
 class VDD extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showLoading: true,
+    };
+  }
 
   componentDidMount() {
-    const renderDOMDate = () => {
+    const renderDOMs = () => {
+      this.setState({
+        showLoading: false,
+      });
+
       const domHTML = `<div>
                          <h2>DOM</h2>
                          <span>Today is ${new Date().toLocaleTimeString()}.</span>
                        </div>`;
 
       document.getElementsByClassName('rp-counter__date--dom')[0].innerHTML = domHTML;
-    };
 
-    this.domDateInterval = setInterval(renderDOMDate, 1000);
-
-    function renderVirtualDOMDate() {
-      const element = (
+      const virtualDOMHTML = (
         <div>
           <h2>Virtual DOM</h2>
           <span>Today is {new Date().toLocaleTimeString()}.</span>
         </div>
       );
 
-      ReactDOM.render(element, document.getElementsByClassName('rp-counter__date--virtual-dom')[0]);
-    }
+      ReactDOM.render(virtualDOMHTML, document.getElementsByClassName('rp-counter__date--virtual-dom')[0]);
+    };
 
-    this.virtualDOMDateInterval = setInterval(renderVirtualDOMDate, 1000);
+    this.state.domInterval = setInterval(renderDOMs, 1000);
   }
 
   componentWillUnmount() {
-    clearInterval(this.virtualDOMDateInterval);
-    clearInterval(this.domDateInterval);
+    clearInterval(this.state.domInterval);
   }
 
   render() {
@@ -44,11 +50,14 @@ class VDD extends Component {
           content="Example of how virtual and normal DOM works, try to inspect the date elements to see it in action. More information can found <a href='https://reactjs.org/docs/rendering-elements.html#react-only-updates-whats-necessary' target='_blank'>here</a>"
         />
 
-        <div className="rp-counter__date--dom">
-          <Loading text="Please wait" />
-        </div>
+        {this.state.showLoading && <Loading text="Please wait" />}
 
-        <div className="rp-counter__date--virtual-dom"></div>
+        {!this.state.showLoading &&
+          <div>
+            <div className="rp-counter__date--dom"></div>
+            <div className="rp-counter__date--virtual-dom"></div>
+          </div>
+        }
       </div>
     );
   }
